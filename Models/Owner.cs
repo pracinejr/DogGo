372f127bcfc6ee9bba415b5b87da271290c75af2
Peter Racine Jr <peterracinejr@gmail.com>
2021-09-21 14:59:47 -0500

    fixed display for neighborhood name

--- Models/Owner.cs
+++ Models/Owner.cs
@@ -11,7 +11,6 @@ namespace DogGo.Models
         public string Email { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
-        public int NeighborhoodId { get; set; }
         public string Phone { get; set; }
         public Neighborhood Neighborhood { get; set; }
     }
